e308a177cb12e86b8f13dfb36c4ab0ac888aa0e5
jallend1 <jallend1@gmail.com>
2020-05-13 00:52:45 -0700

    Begins initial efforts at the API call

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class Fish extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {fish: 'hello'};
+    }
+    catchFish(){
+         fetch('https://acnhapi.com/fish/bitterling')
+         .then(data => data.json())
+         .then(results => {
+            // console.log(results); 
+            this.setState({fish: results.name['name-en']})
+            })
+    }
     render(){
+        this.catchFish();
         return(
             <div>
-                <p>Hello.</p>
+                <p>Hello, {this.state.fish}.</p>
             </div>
         )
     }
