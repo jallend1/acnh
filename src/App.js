210ec8dc1d5d1ee716bed07b758410ddc6448114
jallend1 <jallend1@gmail.com>
2020-05-14 21:46:09 -0700

    Downloads JSON locally to prevent abusing their kind API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import Header from './components/Header';
 import Fish from './components/Fish';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Fish />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    fish: ''
+  }
+
+  catchFish(){
+    
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <Fish />
+      </div>
+    );
+  }
 }
 
 export default App;
